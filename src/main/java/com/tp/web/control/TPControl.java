7e45778d5ceb0d1f8708bc8a9fea510a365de120
Amit.Kumar12 <Amit.Kumar12@SYNPUNDT56TK7R1.synechron.com>
2019-06-19 19:59:08 +0530

    External Tomcat Server Running

--- src/main/java/com/tp/web/control/TPControl.java
+++ src/main/java/com/tp/web/control/TPControl.java
@@ -1,13 +1,18 @@
 package com.tp.web.control;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TPControl {
 
+	Logger LOG = LoggerFactory.getLogger(TPControl.class);
+
 	@RequestMapping("/welcome")
 	public String welcome() {
+		LOG.info("called welcome");
 		return "Hello";
 	}
 
